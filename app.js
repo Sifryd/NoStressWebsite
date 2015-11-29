4d604aac2b386d05a8fdd301d1d9adf2266a1730
Sifryd <aurelien.barthelemy.ab@gmail.com>
2015-11-29 20:14:56 +0100

    gitignore

--- app.js
+++ app.js
@@ -1,16 +1,60 @@
-/**
- * Created by Sifryd on 29/11/2015.
- */
-var app = angular.module('noStressWebsite', []);
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
 
-app.controller('MainCtrl', [
-    '$scope',
-    function($scope){
-        $scope.posts = [
-            {title:"post1", upvotes:5},
-            {title:"post2", upvotes:12},
-            {title:"post3", upvotes:2},
-            {title:"post4", upvotes:7},
-            {title:"post5", upvotes:10}
-        ];
-    }]);
+var routes = require('./routes/index');
+var users = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', routes);
+app.use('/users', users);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+
+module.exports = app;
